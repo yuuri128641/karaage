881c936de8dc4484edd0c0fa41655646bdbf87be
鈴木　優里 <yuri.suzuki@nijibox.co.jp>
2023-07-12 23:48:39 +0900

    fix job date

--- src/utils/createHistoryDate.ts
+++ src/utils/createHistoryDate.ts
@@ -3,7 +3,7 @@ export const createHistoryDate = (date:string | undefined) => {
     const formatDate = date ? new Date(date) : new Date()
 
     const year:number = formatDate.getFullYear();
-    const month:number = formatDate.getMonth();
+    const month:number = formatDate.getMonth() + 1;
 
     const stringDate = `${year}年 ${month}月`
 
